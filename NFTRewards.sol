cf5f1e706f6c13283906cf4836f84ac215232608
jvmez <83326160+jvmez@users.noreply.github.com>
2022-02-07 11:19:20 -0800

    Create NFTRewards.sol

---
+++ NFTRewards.sol
@@ -0,0 +1,149 @@
+// SPDX-License-Identifier: MIT
+
+// Amended by HashLips
+/**
+    !Disclaimer!
+    These contracts have been used to create tutorials,
+    and was created for the purpose to teach people
+    how to create smart contracts on the blockchain.
+    please review this code on your own before using any of
+    the following code for production.
+    HashLips will not be liable in any way if for the use 
+    of the code. That being said, the code has been tested 
+    to the best of the developers' knowledge to work as intended.
+*/
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NFT is ERC721Enumerable, Ownable {
+  using Strings for uint256;
+
+  string baseURI;
+  string public baseExtension = ".json";
+  uint256 public cost = 0.01 ether;
+  uint256 public maxSupply = 10000;
+  bool public paused = false;
+  bool public revealed = false;
+  string public notRevealedUri;
+
+  constructor(
+    string memory _initBaseURI,
+    string memory _initNotRevealedUri
+  ) ERC721("NFT", "NFT") {
+    setBaseURI(_initBaseURI);
+    setNotRevealedURI(_initNotRevealedUri);
+  }
+
+  // internal
+  function _baseURI() internal view virtual override returns (string memory) {
+    return baseURI;
+  }
+
+  // public
+  function mint() public payable {
+    uint256 supply = totalSupply();
+    require(!paused);
+    require(supply + 1 <= maxSupply);
+    require(msg.sender != owner());
+    require(msg.value >= cost);
+
+    address payable giftAddress = payable(msg.sender);
+    uint256 giftValue = 0;
+
+    if(supply > 0) {
+        giftAddress = payable(ownerOf(randomNum(supply, block.timestamp, supply + 1) + 1));
+        giftValue = supply + 1 == maxSupply ? address(this).balance * 10 / 100 : msg.value * 10 / 100;
+    }
+
+    _safeMint(msg.sender, supply + 1);
+
+    if (supply > 0) {
+        (bool success, ) = payable(giftAddress).call{value: giftValue}("");
+        require(success);
+    }
+  }
+
+  function randomNum(uint256 _mod, uint256 _seed, uint256 _salt) public view returns(uint256) {
+      uint256 num = uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender, _seed, _salt))) % _mod;
+      return num;
+  }
+
+  function walletOfOwner(address _owner)
+    public
+    view
+    returns (uint256[] memory)
+  {
+    uint256 ownerTokenCount = balanceOf(_owner);
+    uint256[] memory tokenIds = new uint256[](ownerTokenCount);
+    for (uint256 i; i < ownerTokenCount; i++) {
+      tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
+    }
+    return tokenIds;
+  }
+
+  function tokenURI(uint256 tokenId)
+    public
+    view
+    virtual
+    override
+    returns (string memory)
+  {
+    require(
+      _exists(tokenId),
+      "ERC721Metadata: URI query for nonexistent token"
+    );
+    
+    if(revealed == false) {
+        return notRevealedUri;
+    }
+
+    string memory currentBaseURI = _baseURI();
+    return bytes(currentBaseURI).length > 0
+        ? string(abi.encodePacked(currentBaseURI, tokenId.toString(), baseExtension))
+        : "";
+  }
+
+  //only owner
+  function reveal() public onlyOwner {
+      revealed = true;
+  }
+  
+  function setCost(uint256 _newCost) public onlyOwner {
+    cost = _newCost;
+  }
+  
+  function setNotRevealedURI(string memory _notRevealedURI) public onlyOwner {
+    notRevealedUri = _notRevealedURI;
+  }
+
+  function setBaseURI(string memory _newBaseURI) public onlyOwner {
+    baseURI = _newBaseURI;
+  }
+
+  function setBaseExtension(string memory _newBaseExtension) public onlyOwner {
+    baseExtension = _newBaseExtension;
+  }
+
+  function pause(bool _state) public onlyOwner {
+    paused = _state;
+  }
+ 
+  function withdraw() public payable onlyOwner {
+    // This will pay HashLips 5% of the initial sale.
+    // You can remove this if you want, or keep it in to support HashLips and his channel.
+    // =============================================================================
+    (bool hs, ) = payable(0x943590A42C27D08e3744202c4Ae5eD55c2dE240D).call{value: address(this).balance * 5 / 100}("");
+    require(hs);
+    // =============================================================================
+    
+    // This will payout the owner 95% of the contract balance.
+    // Do not remove this otherwise you will not be able to withdraw the funds.
+    // =============================================================================
+    (bool os, ) = payable(owner()).call{value: address(this).balance}("");
+    require(os);
+    // =============================================================================
+  }
+}
